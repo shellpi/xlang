6689eeb0bf8fca09d5374bd179513d8153fe6946
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-23 14:15:54 +0000

    Update language main file (src/xlang.py)

--- src/xlang.py
+++ src/xlang.py
@@ -1,1 +1,9 @@
-print ('Hello, world')
+import scanner
+
+def __x_code(code: str):
+    interpreter = scanner.Xlang_interpreter()
+    interpreter.scan(code)
+
+def __x_file(path: str):
+    content = open(path).read()
+    __x_code(content)
